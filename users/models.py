b611bcf40507bfe097926609c9f4ea733427b215
RosienVT <rosien.vantoor@gmail.com>
2016-09-13 16:21:59 +0200

    BRUG-46 Person Model

---
+++ users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    food_preferences = models.CharField(max_length=255, blank=True)
+
+    class Meta:
+        abstract = True
