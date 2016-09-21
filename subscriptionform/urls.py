80044ba86088eea4cfa0848f06d673a8f201d2af
RosienVT <rosien.vantoor@gmail.com>
2016-09-21 14:39:33 +0200

    Specified template to use for login. NoReverseMatch error

--- subscriptionform/urls.py
+++ subscriptionform/urls.py
@@ -2,10 +2,12 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth.views import login
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls )),
-    url('^', include('django.contrib.auth.urls')),
+    # url('^', include('django.contrib.auth.urls')), This is the standard django url that handles all authorization
+    url(r'^login/$', login, {'template_name': 'login.html'}),
 ]
 
 if settings.DEBUG and settings.MEDIA_ROOT:
