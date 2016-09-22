86d672f07e0a3691b2025318547be8afec25a7f5
RosienVT <rosien.vantoor@gmail.com>
2016-09-22 14:39:33 +0200

    Found and used the example template for a login page from the Django docs

--- subscriptionform/urls.py
+++ subscriptionform/urls.py
@@ -2,11 +2,12 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth.views import login
+from django.contrib.auth.views import login, password_reset
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login/$', login, {'template_name': 'login.html'}, name='login'),
+    url(r'^password_reset/$', password_reset, name='password_reset'),
 ]
 
 if settings.DEBUG and settings.MEDIA_ROOT:
