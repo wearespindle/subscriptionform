e93dafc91a9475cf287d577aa874083cf61ca385
RosienVT <rosien.vantoor@gmail.com>
2016-10-05 15:03:14 +0200

    Again registered User with Admin

--- users/admin.py
+++ users/admin.py
@@ -2,6 +2,20 @@ from django.contrib import admin
 from .models import MyUser, Coach, Club
 
 
+@admin.register(MyUser)
+class UserAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': ('email', 'date_of_birth',)
+        }),
+        ('More Options', {
+            'classes': ('collapse',),
+            'fields': ('is_staff', 'is_active',)
+        }),
+    )
+    list_display = ('email', 'first_name', 'last_name', 'is_active',)
+
+
 @admin.register(Coach)
 class CoachAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'club', 'is_main_contact',)
