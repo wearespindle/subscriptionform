f67bb191a7488acf6250cf74b27617d9913a4489
RosienVT <rosien.vantoor@gmail.com>
2016-10-07 16:05:49 +0200

    Create SportDetail Model

--- sports/models.py
+++ sports/models.py
@@ -7,23 +7,54 @@ class Participant(Person):
     date_of_birth = models.DateField(_('date of birth'))
     wheelchair_bound = models.BooleanField(_('wheelchair bound'), default=False)
     sport = models.ForeignKey('Sport')
-    sport_details = models.ManyToManyField('SportDetail')
-    team = models.ManyToManyField('Team')
+    sport_details = models.ManyToManyField('SportDetail', blank=True)
+    team = models.ManyToManyField('Team', blank=True)
     # TODO: Come up with a way to automatically assign participants to a club/coach.
 
     class Meta:
         verbose_name = _('Participant')
 
+    def get_full_name(self):
+        return self.first_name + " " + self.last_name
+
+    def __str__(self):
+        return self.get_full_name()
+
 
 class Sport(models.Model):
     name = models.CharField(_('sport'), max_length=30)
-    description = models.CharField(_('description'), max_length=150)
+    description = models.CharField(_('description'), max_length=150, blank=True)
+
+    class Meta:
+        verbose_name = _('Sport')
+
+    def __str__(self):
+        return self.name
+
+
+class Detail(models.Model):
+    """
+    Detail is a model for the individual details.
+    """
+    name_of_detail = models.CharField(_('detail_name'), max_length=30)
+    sport = models.ManyToManyField(Sport, through='SportDetail')
+
+    class Meta:
+        verbose_name = _('Detail')
+
+    def __str__(self):
+        return self.name_of_detail
 
 
 class SportDetail(models.Model):
-    """Sport details belong to a sport and participant and can, for example,
-    be a time, weight class, personal record etc."""
-    pass
+    """SportDetail is an intermediate model between the Sport and Detail models.
+    It can be a time, weight class, personal record etc."""
+    sport = models.ForeignKey(Sport)
+    detail = models.ForeignKey(Detail)
+    value = models.CharField(_('value'), max_length=20)
+
+    def __str__(self):
+        return self.sport + "-" + self.detail
 
 
 class Team(models.Model):
