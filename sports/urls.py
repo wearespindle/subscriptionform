584ee1770d6763c9b3f0180670cb692410ef3236
RosienVT <rosien.vantoor@gmail.com>
2016-11-18 15:28:01 +0100

    Views and templates for teams (CRUD) with filter.

--- sports/urls.py
+++ sports/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 
 from .views import ParticipantCreate, ParticipantDelete, ParticipantDetail, ParticipantList, ParticipantUpdate
+from .views import TeamCreate, TeamDelete, TeamDetail, TeamList, TeamUpdate
 
 urlpatterns = [
 
@@ -10,5 +11,11 @@ urlpatterns = [
     url(r'participants/(?P<pk>\d+)/update/$', ParticipantUpdate.as_view(), name='participant_update'),
     url(r'participants/(?P<pk>[0-9]+)/delete/$', ParticipantDelete.as_view(), name='participant_delete'),
     url(r'participants/(?P<pk>[0-9]+)/delete/confirm/$', ParticipantDelete.as_view(), name='participant_delete_confirm'),
+    url(r'teams/$', TeamList.as_view(), name='teams'),
+    url(r'teams/add/$', TeamCreate.as_view(), name='team_add'),
+    url(r'teams/(?P<pk>\d+)/$', TeamDetail.as_view(), name='team_detail'),
+    url(r'teams/(?P<pk>\d+)/update/$', TeamUpdate.as_view(), name='team_update'),
+    url(r'teams/(?P<pk>[0-9]+)/delete/$', TeamDelete.as_view(), name='team_delete'),
+    url(r'teams/(?P<pk>[0-9]+)/delete/confirm/$', TeamDelete.as_view(), name='team_delete_confirm'),
 
 ]
--- sports/views.py
+++ sports/views.py
@@ -1,8 +1,8 @@
 from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
 from django.urls import reverse_lazy
 
-from .forms import ParticipantForm
-from .models import Participant
+from .forms import ParticipantForm, TeamForm
+from .models import Participant, Team
 
 
 class ParticipantList(ListView):
@@ -16,13 +16,42 @@ class ParticipantDetail(DetailView):
 class ParticipantCreate(CreateView):
     model = Participant
     form_class = ParticipantForm
+    template_name = 'add_form.html'
 
 
 class ParticipantUpdate(UpdateView):
     model = Participant
     form_class = ParticipantForm
+    template_name = 'add_form.html'
 
 
 class ParticipantDelete(DeleteView):
     model = Participant
     success_url = reverse_lazy('participants')
+    template_name = 'confirm_delete.html'
+
+
+class TeamList(ListView):
+    model = Team
+
+
+class TeamDetail(DetailView):
+    model = Team
+
+
+class TeamCreate(CreateView):
+    model = Team
+    form_class = TeamForm
+    template_name = 'add_form.html'
+
+
+class TeamUpdate(UpdateView):
+    model = Team
+    form_class = TeamForm
+    template_name = 'add_form.html'
+
+
+class TeamDelete(DeleteView):
+    model = Team
+    success_url = reverse_lazy('teams')
+    template_name = 'confirm_delete.html'
