c1287669232a69a8a3b7be538ad2ddee676c5592
RosienVT <rosien.vantoor@gmail.com>
2017-02-10 11:21:00 +0100

    Improve and update settings.

--- subscriptionform/settings/local.py
+++ subscriptionform/settings/local.py
@@ -1,50 +1,49 @@
 from subscriptionform.settings.base import *  # noqa
 
 DEBUG = boolean(os.environ.get('DEBUG', 1))
-TEMPLATE_DEBUG = boolean(os.environ.get('DEBUG', DEBUG))
+TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
 
 ALLOWED_HOSTS = []
+CSRF_TRUSTED_ORIGINS = []
 
-# Database
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-# Database connection settings
-DATABASES = {
-    'default': dj_database_url.config(default='postgres://localhost')
-}
+TEMPLATES[0]['OPTIONS']['loaders'] = [
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+]
 
 #############################################
 #     DJANGO DEBUG TOOLBAR CONFIGURATION    #
 #############################################
+if DEBUG:
 
-MIDDLEWARE_CLASSES += (
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-)
+    MIDDLEWARE_CLASSES += (
+        'debug_toolbar.middleware.DebugToolbarMiddleware',
+    )
 
-INSTALLED_APPS += (
-    'debug_toolbar',
-)
+    INSTALLED_APPS += (
+        'debug_toolbar',
+    )
 
-INTERNAL_IPS = ('127.0.0.1', '172.17.42.1',)
+    INTERNAL_IPS = ('127.0.0.1', '172.17.42.1',)
 
-DEBUG_TOOLBAR_PANELS = (
-    'debug_toolbar.panels.versions.VersionsPanel',
-    'debug_toolbar.panels.timer.TimerPanel',
-    'debug_toolbar.panels.settings.SettingsPanel',
-    'debug_toolbar.panels.headers.HeadersPanel',
-    'debug_toolbar.panels.request.RequestPanel',
-    'debug_toolbar.panels.sql.SQLPanel',
-    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-    'debug_toolbar.panels.templates.TemplatesPanel',
-    'debug_toolbar.panels.cache.CachePanel',
-    'debug_toolbar.panels.signals.SignalsPanel',
-    'debug_toolbar.panels.logging.LoggingPanel',
-    'debug_toolbar.panels.profiling.ProfilingPanel',
-    'debug_toolbar.panels.redirects.RedirectsPanel',
-)
+    DEBUG_TOOLBAR_PANELS = (
+        'debug_toolbar.panels.versions.VersionsPanel',
+        'debug_toolbar.panels.timer.TimerPanel',
+        'debug_toolbar.panels.settings.SettingsPanel',
+        'debug_toolbar.panels.headers.HeadersPanel',
+        'debug_toolbar.panels.request.RequestPanel',
+        'debug_toolbar.panels.sql.SQLPanel',
+        'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+        'debug_toolbar.panels.templates.TemplatesPanel',
+        'debug_toolbar.panels.cache.CachePanel',
+        'debug_toolbar.panels.signals.SignalsPanel',
+        'debug_toolbar.panels.logging.LoggingPanel',
+        'debug_toolbar.panels.profiling.ProfilingPanel',
+        'debug_toolbar.panels.redirects.RedirectsPanel',
+    )
 
-DEBUG_TOOLBAR_CONFIG = {
-    'SHOW_TEMPLATE_CONTEXT': True,
-}
+    DEBUG_TOOLBAR_CONFIG = {
+        'SHOW_TEMPLATE_CONTEXT': True,
+    }
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- subscriptionform/urls.py
+++ subscriptionform/urls.py
@@ -1,7 +1,6 @@
-from django.conf import settings
 from django.conf.urls import include, url
-from django.conf.urls.static import static
 from django.contrib import admin
+
 from home.views import index, MenuView
 
 
@@ -15,7 +14,3 @@ urlpatterns = [
     url(r'^menu/$', MenuView.as_view(), name='menu')
 
 ]
-
-if settings.DEBUG and settings.MEDIA_ROOT:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
