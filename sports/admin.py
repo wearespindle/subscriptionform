c4d8cb7a7077b3f6310e7d31c16e331040dc1a0d
RosienVT <rosien.vantoor@gmail.com>
2017-02-24 14:06:29 +0100

    Export functionality for Participant, Coach, and Team. Needs field validation.

--- sports/admin.py
+++ sports/admin.py
@@ -1,15 +1,51 @@
 from django.contrib import admin
 from .models import Coach, Participant, Sport, Team, Discipline, Performance
 
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
+
+class ParticipantResource(resources.ModelResource):
+    """
+    Class used for the export of a list of registered participants.
+    """
+    class Meta:
+        model = Participant
+        # The fields that are used for the export.
+        fields = ('first_name', 'prefix', 'last_name', 'date_of_birth', 'gender', 'food_preferences',
+                  'club__name', 'disciplines', 'disciplines__performance',)
+        
+
+class CoachResource(resources.ModelResource):
+    """
+    Class used for the export of a list of registered coaches.
+    """
+    class Meta:
+        model = Coach
+        # The fields that are used for the export.
+        fields = ('club__name', 'first_name', 'prefix', 'last_name', 'gender',
+                  'phone_number', 'email', 'food_preferences', )
+
+
+class TeamResource(resources.ModelResource):
+    """
+    Class used for the export of teams.
+    """
+    class Meta:
+        model = Team
+        # The fields that are used for the export.
+        fields = ('club__name', 'team_name', 'team_members')
+
 
 @admin.register(Participant)
-class ParticipantAdmin(admin.ModelAdmin):
+class ParticipantAdmin(ImportExportModelAdmin):
     list_display = ('first_name', 'last_name', 'club',)
     list_filter = ('disciplines', 'club', 'wheelchair_bound',)
+    resource_class = ParticipantResource
 
 
 @admin.register(Coach)
-class MyCoachAdmin(admin.ModelAdmin):
+class MyCoachAdmin(ImportExportModelAdmin):
     list_display = ('first_name', 'last_name', 'email', 'club')
 
 
@@ -19,7 +55,7 @@ class SportAdmin(admin.ModelAdmin):
 
 
 @admin.register(Team)
-class TeamAdmin(admin.ModelAdmin):
+class TeamAdmin(ImportExportModelAdmin):
     list_display = ('team_name', 'club',)
 
 
